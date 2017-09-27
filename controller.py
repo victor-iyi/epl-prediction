2510667dc26c40f5195d9a30a2c3d99cb49b7732
victor-iyiola <javafolabi@gmail.com>
2017-09-27 17:26:16 +0100

    Controller and templates

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
