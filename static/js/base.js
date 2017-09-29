63b032727368fa562e11c2cb9cda564f29037a71
tomiiide <ayotomiiide@gmail.com>
2017-09-29 17:52:34 +0100

    API Hookup in progress

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,37 @@
+
 new Vue({
   el : '.page-container',
   data :  {
-    result : ''
+    result : '',
+    home_team: '',
+    away_team: ''
   },
   methods : {
     open : function(){
       this.result = '';
+    },
+    predict : function(){
+      if(this.away_team == this.home_team){
+        alert('Cannot compare');
+      }
+      axios.post('/__predict', {
+        away_team: this.away_team,
+        home_team: this.home_team
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
   },
   computed : {
     modalOpen : function(){
       return this.result.length <= 0;
     }
+  },
+  created : function(){
+    Window.App = this;
   }
 });
