c442e4f623d8dfc42ad93a5b8baedb1e2ef372a8
tomiiide <ayotomiiide@gmail.com>
2017-09-29 18:53:53 +0100

    Front end hook up done

--- static/js/base.js
+++ static/js/base.js
@@ -11,15 +11,16 @@ new Vue({
       this.result = '';
     },
     predict : function(){
-      if(this.away_team == this.home_team){
-        alert('Cannot compare');
+      if(this.away_team === this.home_team){
+        alert('Cannot compare same teams');
+        return;
       }
       axios.post('/__predict', {
         away_team: this.away_team,
         home_team: this.home_team
       })
       .then(function (response) {
-        console.log(response);
+        Window.App.result = response.data.response.data.result;
       })
       .catch(function (error) {
         console.log(error);
@@ -29,6 +30,19 @@ new Vue({
   computed : {
     modalOpen : function(){
       return this.result.length <= 0;
+    },
+    winner: function(){
+      if(this.result === 'A') return this.away_team+" is likely to win";
+      if(this.result === 'H') return this.home_team+" is likely to win";
+      if(this.result === 'D') return "Match is likely to be a draw ";
+    },
+    winnerletter : function(){
+      if(this.result === 'A' || this.result === 'H') return "W";
+      if(this.result === 'D') return "D";
+    },
+    winnerclass : function(){
+      if(this.result === 'A' || this.result === 'H') return "win";
+      if(this.result === 'D') return "draw";
     }
   },
   created : function(){
